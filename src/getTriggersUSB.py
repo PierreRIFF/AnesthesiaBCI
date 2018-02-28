2502b7feadadd61b8e921c9385c93de68b1c1ff5
HanBnrd <johann.benerradi@gmail.com>
2018-02-28 22:14:14 +0100

    Triggers from USB device

---
+++ src/getTriggersUSB.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Get triggers from USB device
+"""
+
+import usb.core
+import usb.util
+import sys
+import array as a
+
+
+# Hexadecimal vendor and product values (remove '0x' if decimal)
+device = usb.core.find(idVendor=0x****, idProduct=0x****)
+print(device)
+
+endpoint = device[0][(1,0)][0] # first endpoint
+# device[configIndex][(interfaceIndex,interfaceAlt)][endpointIndex]
+
+## Device configuration
+print('********************')
+print('Configuration ...')
+c = 1
+for config in device:
+    print('Config # ', c)
+    print('Interfaces : ', config.bNumInterfaces)
+    for i in range(config.bNumInterfaces):
+        if device.is_kernel_driver_active(i):
+            try:
+                device.detach_kernel_driver(i)
+            except usb.core.USBError as e:
+                sys.exit("Could not detatch kernel driver: %s" % str(e))
+        print('Interface # ', i)
+    c+=1
+
+# Set configuration
+try:
+    device.reset()
+    device.set_configuration()
+except usb.core.USBError as e:
+    sys.exit("Could not set configuration: %s" % str(e))
+print('Done !')
+print('********************')
+
+## Read a data packet
+print('Triggers :')
+collected = 0
+attempts = 10
+data = None
+while collected < attempts :
+    try:
+        data = device.read(endpoint.bEndpointAddress,endpoint.wMaxPacketSize)
+        collected += 1
+        if data != a.array('B', [0, 0, 0]):
+            print(data)
+    except usb.core.USBError as e:
+        data = None
+        if e.args == ('Operation timed out',):
+            continue
+
+## Release device
+print('********************')
+print('Releasing device ...')
+c = 1
+for config in device:
+    print('Config # ', c)
+    print('Interfaces : ', config.bNumInterfaces)
+    for i in range(config.bNumInterfaces):
+        # Release the device
+        usb.util.release_interface(device, i)
+        # Reattach the device to the OS kernel
+        device.attach_kernel_driver(i)
+        print('Interface # ', i)
+    c+=1
+print('Done !')
+print('********************')
